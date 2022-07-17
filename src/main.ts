ff7321e2d10f6c3b76c19501c9bd56a024640c3b
IvanPratasevich <iv.pratasevich@gmail.com>
2022-07-17 20:46:29 +0300

    fix: fix yaml

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT);
 }
 bootstrap();
