bc9c4ac7f8af026d39b495ffd5ed1cc08791a119
IvanPratasevich <iv.pratasevich@gmail.com>
2022-07-17 20:51:00 +0300

    refactor: code refactor

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { readFile } from 'fs/promises';
 import { parse } from 'yaml';
 import { SwaggerModule } from '@nestjs/swagger';
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4444;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +15,9 @@ async function bootstrap() {
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
-
   SwaggerModule.setup('doc', app, document);
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    console.log(`Start server on the ${PORT} port!`);
+  });
 }
 bootstrap();
